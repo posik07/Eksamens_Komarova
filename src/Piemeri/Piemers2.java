5e360bd313d5b95b27e0b094944b07d9d13e7a69
Komarova <posja2007@gmail.com>
2025-06-10 21:50:21 +0300

    Pievienoja apakšklase Suns ar konstruktoru, arī papildina ar specialo metode

--- src/Piemeri/Piemers2.java
+++ src/Piemeri/Piemers2.java
@@ -2,7 +2,7 @@ package Piemeri;
 // Piemērs 2: Mantošana
 
 public class Piemers2 {
-	
+	//Bāzes klase(vecāku klase)
 	static class Dzivnieks {
 		private String vards;
 		private int vecums;
@@ -26,6 +26,33 @@ public class Piemers2 {
 		 public int getVecums() {
 			 return vecums;
 		 }
+		 
+		 // Publiska metode ēšanai un gulēšanai
+		 public void est() {
+	            System.out.println(vards + " ēd.");
+	        }
+	        
+	        
+	        public void gulet() {
+	            System.out.println(vards + " guļ.");
+	        }
+	        
+	        //Apakšklase(bērnu klase)
+	        // Suns manto no Dzivnieks
+	        static class Suns extends Dzivnieks{
+	        	private String skirne; //papildu īpašība, kas ir tikai suņiem
+	        	
+	        	//Konstruktors apakšklase
+	        	public Suns(String vards, int vecums, String skirne) {
+	        		super(vards, vecums); // super izsauc vecaku klases konstruktors
+	        		this.skirne = skirne;
+	        	}
+	        	
+	        	//specifiska metode tikai suņiem
+	        	public void riet() {
+	        		System.out.println(getVards() + " rej: Gaf-gaf!! >:O");
+	        	}
+	        }
 	}
 
 }
